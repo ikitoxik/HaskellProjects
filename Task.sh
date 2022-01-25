4be35b4acb679f4c6354c605b694d0c5feaa17eb
Nikita Butyrin <94044348+ikitoxik@users.noreply.github.com>
2022-01-25 15:04:37 +0300

    Update Task.sh

--- Task.sh
+++ Task.sh
@@ -1,1 +1,10 @@
-awk -F " " '{print $1 " " $2 " " $1+$2}' file.txt>>file1.txt
+while read y
+do
+i=`expr index "$y" " "`
+let i=$i-1
+let a=`echo $y | cut -b 1-$i`
+let i=$i+2
+let b=`echo $y | cut -b $i-${#y}`
+let c=$a+$b
+echo "$a  $b  $c">>file1.txt
+done < $1
